e288d9c17fc9559bbfec033e3b1f8f16d7b9d9f3
soyoung <0001401@naver.com>
2022-11-12 01:49:28 +0900

    add scheduleCreate making

--- src/pages/schedule/component/ScheduleCard.jsx
+++ src/pages/schedule/component/ScheduleCard.jsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 const ScheduleCard = () => {
+  const seheduleList = useSelector((state) => state);
+  console.log(seheduleList);
+  const dispatch = useDispatch();
   return <div>ScheduleCard</div>;
 };
 
