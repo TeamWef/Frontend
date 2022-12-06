be1a35571cc6efeb04e34bb0a3144bd391ee45d4
soyoung <0001401@naver.com>
2022-12-06 23:48:56 +0900

    Update kakao url

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,7 @@
 export const ServerUrl = process.env.REACT_APP_BASE_URL;
 
 //kakao login
-const REDIRECT_URI =
-  "https://main.daegm2i4mn3we.amplifyapp.com//members/kakao/callback";
+const REDIRECT_URI = "https://happyseptember.shop/members/kakao/callback";
 const REST_API_KEY = "e19d1426333e06b682ab242673374c0f";
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
