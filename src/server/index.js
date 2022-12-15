1ca3989486c8d968ff309552784e64c5ff6a2e06
Yboda <ybr2265@naver.com>
2022-12-16 00:43:25 +0900

    Modify Google Login

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ const K_REDIRECT_URI =
 const REST_API_KEY = "e19d1426333e06b682ab242673374c0f";
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${K_REDIRECT_URI}&response_type=code`;
 
-//google login
+///google login
 const G_REDIRECT_URI =
   "https://main.daegm2i4mn3we.amplifyapp.com/members/google/callback";
 export const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=1021733699851-sf89i152954bqbhggalaemk9mu6imsbo.apps.googleusercontent.com&redirect_uri=${G_REDIRECT_URI}&response_type=code&scope=email profile`;
